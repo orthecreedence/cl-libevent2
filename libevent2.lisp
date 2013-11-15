f09b06c7a7f0917eda49578d1d35cc5ded2ea655
Camille Troillard <camille@osculator.net>
2013-11-15 14:02:02 +0100

    Move OS X library loading paths from the :unix spec to :darwin.

--- libevent2.lisp
+++ libevent2.lisp
@@ -10,13 +10,14 @@
 
 (eval-when (:load-toplevel)
   (define-foreign-library libevent2-core
-    (:darwin (:or "libevent_core.dylib"))
+    (:darwin (:or
+              "libevent_core.dylib"
+              ; brew's install of libevent on Mac OX X
+              "/usr/local/lib/libevent_core.dylib"))
     (:unix (:or "libevent_core.so"
                 "libevent_core-2.0.so.5"
                 "/usr/lib/libevent_core.so"
-                "/usr/local/lib/libevent_core.so"
-                ; brew's install of libevent on Mac OX X
-                "/usr/local/lib/libevent_core.dylib"))
+                "/usr/local/lib/libevent_core.so"))
     (:windows (:or "libevent_core.dll"
                    "libevent_core-2-0-5.dll"))
     (t (:default "libevent_core")))
@@ -24,13 +25,13 @@
     (use-foreign-library libevent2-core))
 
   (define-foreign-library libevent2-extra
-    (:darwin (:or "libevent_extra.dylib"))
+    (:darwin (:or "libevent_extra.dylib"
+                ; brew's install of libevent on Mac OX X
+              "/usr/local/lib/libevent_extra.dylib"))
     (:unix (:or "libevent_extra.so"
                 "libevent_extra-2.0.so.5"
                 "/usr/lib/libevent_extra.so"
-                "/usr/local/lib/libevent_extra.so"
-                ; brew's install of libevent on Mac OX X
-                "/usr/local/lib/libevent_extra.dylib"))
+                "/usr/local/lib/libevent_extra.so"))
     (:windows (:or "libevent_extra.dll"
                    "libevent_extra-2-0-5.dll"))
     (t (:default "libevent_extra")))
